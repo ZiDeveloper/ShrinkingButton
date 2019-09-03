760d72b1f0b87db6c4103b803d1b38b116776036
Fabian Zi <ZiDeveloper@users.noreply.github.com>
2019-09-03 13:07:35 +0200

    Proper versioning
    1.0.0.1122 -> 1.2.1

--- src/main/java/com/github/zideveloper/shrinkingbutton/Main.java
+++ src/main/java/com/github/zideveloper/shrinkingbutton/Main.java
@@ -18,7 +18,7 @@ public class Main
 {
     public static final String MODID = "shrinkingbutton";
     public static final String NAME = "Shrinking Button Addon";
-    public static final String VERSION = "1.2-1.12.2";
+    public static final String VERSION = "1.2.1";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String[] CREDITS = {"Camellias_", "zabi94", "MineMaarten"};
     
